c0f7605d65abcfa444e3f8f07ecb23333e7dbae6
Stealthoneill <35182630+Stealthoneill@users.noreply.github.com>
2019-04-08 22:53:28 +0100

    Code Change
    Tidy up code

--- caller-bot.py
+++ caller-bot.py
@@ -1,19 +1,23 @@
 import discord
 
+TOKEN = 'NTY0OTEwODQzOTMwNzM4Njg5.XKuwjA.cT9yJiBR2o0Kggb9eVAWSj6LHf0'
+
 client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
     print("This bot is ready to go!")
     await client.change_presence(game=discord.Game(name="Making a bot"))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == "Hello":
-        await client.send_message(message.channel, "World")
 
-
-
-        client.run('NTY0OTEwODQzOTMwNzM4Njg5.XKuwjA.cT9yJiBR2o0Kggb9eVAWSj6LHf0')
+client.run(TOKEN)
