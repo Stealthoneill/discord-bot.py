5238870f52b95303c9629ded855e0e7fc3b6e942
Stealthoneill <35182630+Stealthoneill@users.noreply.github.com>
2019-04-09 00:39:20 +0100

    Update caller-bot.py

--- caller-bot.py
+++ caller-bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 
 TOKEN = 'NTY0OTEwODQzOTMwNzM4Njg5.XKu-Bg.r0AKB7QWEsIkVlcK2l921wi5FPQ'
 
@@ -12,6 +13,10 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
+
+    if message.content.startswith('!roll'):
+        print("You rolled for a {}").format(random.randint(1,20))
+        await client.send_message(message.channel)
 
 @client.event
 async def on_ready():
